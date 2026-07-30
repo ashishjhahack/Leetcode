31ee0c6d8465e7df32423cac0d4b000ab6399289
ashishjhahack <ashishjhag143@gmail.com>
2026-07-30 11:46:56 +0530

    Time: 0 ms (100%), Space: 44.9 MB (29.16%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,18 +1,19 @@
 class Solution {
-    public int searchInsert(int[] nums, int target) {
+    public int searchInsert(int[] nums, int target) {   // lower bound approach
         int n= nums.length;
         int i = 0, j = n-1;
-
+        int ans = n;
+        
         while(i <= j){
-            int mid = i + (j - i)/2;
-            if(nums[mid] == target) return mid;
-            else if(nums[mid] < target){
-                i = mid+1;
-            }
-            else{
-                j = mid-1;
-            }
+           int mid = i + (j-i)/2;
+           if(nums[mid] >= target){
+            ans = mid;
+            j = mid-1;
+           } 
+           else{
+            i = mid+1;
+           }
         }
-        return i;   // If target not found
+        return ans;
     }
 }
