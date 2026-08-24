9c2b3d22cb207edf8cbb435ad1315dfe3093a211
ashishjhahack <ashishjhag143@gmail.com>
2026-08-24 23:13:11 +0530

    Time: 619 ms (27.75%), Space: 0B (100%) - LeetHub

--- 2356-number-of-unique-subjects-taught-by-each-teacher/2356-number-of-unique-subjects-taught-by-each-teacher.sql
+++ 2356-number-of-unique-subjects-taught-by-each-teacher/2356-number-of-unique-subjects-taught-by-each-teacher.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-select teacher_id, count(distinct subject_id) cnt
-from Teacher
-group by teacher_id;
+SELECT teacher_id, COUNT(DISTINCT subject_id) cnt
+FROM Teacher
+GROUP BY teacher_id;
