efbfa8fd749c9b24008639d9be247f0ae3ee15f2
Ashish <ashishjhag143@gmail.com>
2026-08-27 21:06:17 +0530

    Time: 6 ms (87.22%), Space: 45.6 MB (10.11%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        // RPN :-  eg ["10", "2", "+"]  => 10+2 = 12
+        // Basic Approach: 
+        // 1. if find operand then push it into stack
+        // 2. else find operator then perform the operation and then push the result into stack
+        Stack<Integer> st = new Stack<>();
+        for(String token: tokens){
+            if(!token.equals("+") && !token.equals("-") && !token.equals("*") && !token.equals("/")){    // if number(operand) then push it into stack
+                st.push(Integer.parseInt(token));
+            }
+            else{   // if find operator
+                int b = st.pop();   // first pop the right operand
+                int a = st.pop();   // then right
+                int res = 0;
+                if(token.equals("+")) res = a+b;
+                else if(token.equals("-")) res = a-b;
+                else if(token.equals("*")) res = a*b;
+                else if(token.equals("/")) res = a/b;
+
+                // push the res back into stack
+                st.push(res);
+            }
+        }
+        return st.peek();
+    }
+}
