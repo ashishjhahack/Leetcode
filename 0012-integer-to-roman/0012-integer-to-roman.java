82a83621ee0fe7ec3ef4963afae5cf36df0c776a
Ashish <ashishjhag143@gmail.com>
2026-09-05 16:37:52 +0530

    Time: 3 ms (99.8%), Space: 46.4 MB (42.59%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,11 +1,17 @@
 class Solution {
     public String intToRoman(int num) {
-        String[] thousands = {"", "M", "MM", "MMM"};
-        String[] hundreds = {"", "C", "CC", "CCC", "CD","D", "DC", "DCC", "DCCC", "CM"};
-        String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
-        String[] ones = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbol = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
-        // Eg. (1249%1000) = 249
-        return thousands[num/1000]+hundreds[(num%1000)/100]+tens[(num%100)/10]+ones[num%10];
+        StringBuilder ans = new StringBuilder();
+        
+        for(int i=0; i<values.length; i++){
+            // use this b/c there will multiple "MMM"
+            while(num >= values[i]){ // Eg. num = 3749. So, 3000 > 1000
+                num -= values[i];   // num = 2749
+                ans.append(symbol[i]);   // append "M (1000)"
+            }   // 2749 > 1000 then again add
+        }
+        return ans.toString();
     }
 }
